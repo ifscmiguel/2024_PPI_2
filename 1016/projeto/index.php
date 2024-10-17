fa2f0929cddac581ca81321afdf55aea2eb8ce7f
Miguel Zarth <126920753+ifscmiguel@users.noreply.github.com>
2024-10-16 21:41:09 -0300

    aula 16/10/2024

---
+++ 1016/projeto/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+    <div class="container">
+        <form action="salvar.php" method="post">
+            <input required type="text" name="nome" id="nome" placeholder="Nome">
+            <input required type="text" name="msg" id="msg" placeholder="Mensagem">
+            <input type="submit" value="enviar">
+        </form>
+        <div class="mensagens">
+            <?=file_get_contents('mensagens.txt');?>
+        </div>
+    </div>
+</body>
+</html>
---
+++ 1016/receber_dados.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Receber Dados do Form</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <h1>Dados do Form</h1>
+        <!-- aqui vai o PHP -->
+        <?php
+        /*
+        Você pode receber via post ou via get usando:
+        $_POST ou $_GET
+        */
+        echo 'Nome: ' . $_POST['nome'];
+        echo '<br>';
+        echo 'Estado: ' . $_POST['estado'];
+        echo '<br>';
+        echo 'Campus: ' . $_GET['campus'];
+        ?>
+
+        <h1>Receber dados com segurança</h1>
+        <?php
+        $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+        $campus = filter_input(INPUT_GET, 'campus', FILTER_SANITIZE_SPECIAL_CHARS);
+        echo "Nome: $nome <br> Campus: $campus";
+        ?>
+    </div>
+</body>
+
+</html>
